dc04e586b31b6b1fdcc5d28f0b1c4243f071cb81
madhu <madhu@asus>
2024-05-22 16:24:34 +0530

    this is my first commit messege

---
+++ proj01/src/com/pack1/Example.java
@@ -0,0 +1,12 @@
+package com.pack1;
+
+public class Example {
+
+	public void wish()
+	{
+		System.out.println("Example.wish() before updating");
+	}
+	
+	
+	
+}
---
+++ proj02/src/com/pack1/Mobile.java
@@ -0,0 +1,12 @@
+package com.pack1;
+
+public class Mobile {
+
+	
+	
+	public void camera()
+	{
+		System.out.println("Mobile.camera()");
+	}
+	
+}
